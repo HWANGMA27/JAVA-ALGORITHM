998639dff43a8fcc2d00c9e90bd62e3e2ffd5fb6
hwangma27 <pikacu22@kakao.com>
2021-11-22 19:56:06 +0900

    [ADD] Count bursted puppets using queue

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import Solution.Common;
 import Solution.Hash.*;
 import Solution.SlidingWindow.MaxSalesProfit;
 import Solution.SlidingWindow.Subsequence;
+import Solution.Stack.PuppetDraw;
 import Solution.Stack.RemoveCharInsideBracket;
 import Solution.Stack.RightBrackets;
 import Solution.String.*;
@@ -14,7 +15,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Common solution = new RemoveCharInsideBracket();
+        Common solution = new PuppetDraw();
         solution.solution();
         return;
     }
