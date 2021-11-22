5272ddcaf835665a730d560b5bcf495d98b1d838
hwangma27 <pikacu22@kakao.com>
2021-11-22 20:13:23 +0900

    [TEST COMMIT]

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        //test
         Common solution = new PuppetDraw();
         solution.solution();
         return;
