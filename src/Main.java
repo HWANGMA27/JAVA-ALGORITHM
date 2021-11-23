0fb0d11c3c21ed106095126ede8192b8af7ce81d
hwangma27 <pikacu22@kakao.com>
2021-11-23 23:09:13 +0900

    [ADD] Algorithm using queue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Solution.Common;
 import Solution.Queue.PuppetDraw;
 import Solution.Stack.DivideByLaser;
+import Solution.Stack.SavePrincess;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new DivideByLaser();
+        Common solution = new SavePrincess();
         solution.solution();
         return;
     }
