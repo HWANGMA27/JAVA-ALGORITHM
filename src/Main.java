5d77ad2ab172fb92a170db854259dbebcc2e38c6
hwangma27 <pikacu22@kakao.com>
2021-11-27 21:34:18 +0900

    [ADD] Selection Sort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Solution.Common;
 import Solution.Queue.Curriculum;
 import Solution.Queue.SavePrincess;
+import Solution.Sort.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new Curriculum();
+        Common solution = new SelectionSort();
         solution.solution();
         return;
     }
