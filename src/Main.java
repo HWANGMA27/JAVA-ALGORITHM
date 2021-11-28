8ca77eabf525f1c72b7019c7a3f5a5f18eb0159b
hwangma27 <pikacu22@kakao.com>
2021-11-28 18:41:37 +0900

    [ADD] Bubble Sort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import Solution.Common;
 import Solution.Queue.Curriculum;
 import Solution.Queue.SavePrincess;
+import Solution.Sort.BubbleSort;
 import Solution.Sort.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new SelectionSort();
+        Common solution = new BubbleSort();
         solution.solution();
         return;
     }
