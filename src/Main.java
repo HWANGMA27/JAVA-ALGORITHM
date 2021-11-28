bd2c56a72c41f547dfe3e6bd4c5456df3b37edaa
hwangma27 <pikacu22@kakao.com>
2021-11-28 19:10:51 +0900

    [ADD] Insertion Sort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import Solution.Common;
 import Solution.Queue.Curriculum;
 import Solution.Queue.SavePrincess;
 import Solution.Sort.BubbleSort;
+import Solution.Sort.InsertionSort;
 import Solution.Sort.SelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new BubbleSort();
+        Common solution = new InsertionSort();
         solution.solution();
         return;
     }
