ac91a4314c851fa371bf7fb551274208dab307f0
hwangma27 <pikacu22@kakao.com>
2021-12-01 15:02:18 +0900

    [ADD] Counting Priority Using Queue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Solution.Common;
 import Solution.Dequeue.LRU;
+import Solution.Queue.EmergencyRoom;
 import Solution.Sort.LRUSelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new LRUSelectionSort();
+        Common solution = new EmergencyRoom();
         solution.solution();
         return;
     }
