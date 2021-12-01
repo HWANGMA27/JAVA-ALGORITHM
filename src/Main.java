8c36c7404e7d39f8232841e0c7d86615014c4677
hwangma27 <pikacu22@kakao.com>
2021-12-01 13:49:40 +0900

    [ADD] Least Recently Used Algorithm Using Selection Sort

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import Solution.Common;
 import Solution.Dequeue.LRU;
+import Solution.Sort.LRUSelectionSort;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new LRU();
+        Common solution = new LRUSelectionSort();
         solution.solution();
         return;
     }
