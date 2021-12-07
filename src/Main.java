5f82cdd5a43b358cd839c4f96724b93584b2928c
hwangma27 <pikacu22@kakao.com>
2021-12-07 20:01:07 +0900

    [ADD] Sorting implement comparable interface

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Solution.Hash.CheckDuplicate;
 import Solution.Common;
 import Solution.Sort.FindNaughtiness;
+import Solution.Sort.SortCoordinates;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new FindNaughtiness();
+        Common solution = new SortCoordinates();
         solution.solution();
         return;
     }
