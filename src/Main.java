6c6d046a1a39790afce13728d50a863989726b76
hwangma27 <pikacu22@kakao.com>
2021-12-15 21:04:18 +0900

    [ADD] Apply Decision algorithm using binary search 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import Solution.Hash.CheckDuplicate;
 import Solution.Common;
-import Solution.Sort.BinarySearch;
-import Solution.Sort.DecisionAlgorithm;
-import Solution.Sort.FindNaughtiness;
-import Solution.Sort.SortCoordinates;
+import Solution.Sort.*;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new DecisionAlgorithm();
+        Common solution = new DecisionAlgorithm2();
         solution.solution();
         return;
     }
