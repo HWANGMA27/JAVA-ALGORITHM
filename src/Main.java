1c957885decf11ce1523562eada91e50c2da3ed2
hwangma27 <pikacu22@kakao.com>
2021-12-26 18:59:23 +0900

    [ADD] Print all subset using DFS

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import Solution.Common;
+import Solution.DFS.BinaryTree;
 import Solution.Recursive.Basic.FactorialRecursive;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new FactorialRecursive();
+        Common solution = new BinaryTree();
         solution.solution();
         return;
     }
