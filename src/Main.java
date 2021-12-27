ac2eb8a66c2ebdeb04f1195291cbea5783ccb348
hwangma27 <pikacu22@kakao.com>
2021-12-27 17:51:38 +0900

    [ADD] Find calf using BFS

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Solution.BFS.BinaryTree;
+import Solution.BFS.findCalf;
 import Solution.Common;
 import Solution.Recursive.Basic.FactorialRecursive;
 
 public class Main {
     public static void main(String[] args) {
         //test
-        Common solution = new BinaryTree();
+        Common solution = new findCalf();
         solution.solution();
         return;
     }
